99f1141bf4757ee58890037712c6416827dc4712
CappelleJess <cappellejessica.lcj@gmail.com>
2020-10-21 15:21:52 +0200

    datepicker

--- application/views/pages/inscription.php
+++ application/views/pages/inscription.php
@@ -110,12 +110,9 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
     </head>
         <body>
         <form method="post">
@@ -139,10 +136,10 @@
                         <option value="4">Autre</option>
                     </select>
                 </div>
-                <section class="col-sm-6">
+                <section class="col-sm-1">
                     <div class="form-group">
                         <div class="input-group date" data-provide="datepicker">
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" id="date" placeholder="JJ/MM/AAAA">
                             <span class="input-group-addon">
                                 <span class="glyphicon glyphicon-calendar"></span>
                             </span>
@@ -154,27 +151,6 @@
                         $('.datepicker').datepicker();
                     });
                 </script>
-                    <select name="mois" required>
-                        <?php 
-                            for($i = 1; $i<= 12; $i++){
-                        ?>
-                        <option style="display:none;" selected>Mois</option>
-                        <option value="Mois; <?= $i ?>"><?= $i ?></option>
-                        <?php
-                            }
-                        ?>
-                    </select>
-                    <select name="annee" required>
-                        <?php 
-                            for($i = 1960; $i<= 2002; $i++){
-                        ?>
-                        <option style="display:none;" selected>Année</option>
-                        <option value="Année; <?= $i ?>"><?= $i ?></option>
-                        <?php
-                            }
-                        ?>
-                    </select>
-                </div>                
                 <div class="form-group">
                     <?php
                         if(isset($err_lieu)){
