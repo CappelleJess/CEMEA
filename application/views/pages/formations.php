69b761df685958a44b638253fae5d945c48a2138
CappelleJess <cappellejessica.lcj@gmail.com>
2020-10-28 11:09:39 +0100

    tryin modal

--- application/views/pages/formations.php
+++ application/views/pages/formations.php
@@ -9,7 +9,7 @@
 				<div class="container">
 					<form method="post">
 						<div class="container" style="width: 200px;">
-                			<button type="signup" class="btn btn-success"><a href="<?php echo site_url('inscription'); ?>" style="color: white;">S'inscrire</a></button>
+                			<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg"><a href="<?php echo site_url('inscription'); ?>" style="color: white;">S'inscrire</a></button>
                 			<button type="submit" onclick="history.back()" class="btn btn-danger">Retour</button>
             			</div>
 					</form>
