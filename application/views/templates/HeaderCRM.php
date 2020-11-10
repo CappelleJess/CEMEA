e9eb4a919d03f8b0635c798296fe1c378b33e275
CappelleJess <cappellejessica.lcj@gmail.com>
2020-11-10 12:49:43 +0100

    CRM

---
+++ application/views/templates/HeaderCRM.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    	<meta name="description" content="">
+    	<meta name="author" content="">
+    	<link rel="icon" href="../../../../favicon.ico">
+    	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
+    	<script type="text/javascript" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    	
+			
+			<title>CEMEA CRM</title>
+
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="jumbotron.css" rel="stylesheet">
+	</head>
+		<body>
+		
+
+			<script src="jquery-3.3.1.min.js"></script>
+			<script src="bootstrap/js/bootstrap.min.js"></script>
+		</body>
+</html>
