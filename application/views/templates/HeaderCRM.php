dd6f2810e481f57f4c63c8b5d23e5a3df553b5c4
CappelleJess <cappellejessica.lcj@gmail.com>
2020-11-10 15:35:23 +0100

    Update HeaderCRM.php

--- application/views/templates/HeaderCRM.php
+++ application/views/templates/HeaderCRM.php
@@ -16,7 +16,40 @@
         <link href="jumbotron.css" rel="stylesheet">
 	</head>
 		<body>
-		
+			<h1><?php echo $title; ?></h1>
+			    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      				<a class="navbar-brand" href="#">CEMEA-CRM</a>
+      				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        				<span class="navbar-toggler-icon"></span>
+      				</button>
+
+      					<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        					<ul class="navbar-nav mr-auto">
+          						<li class="nav-item active">
+            						<a class="nav-link" href="home">Tableau de bord<span class="sr-only">(current)</span></a>
+          						</li>
+          						<li class="nav-item">
+          							<a class="nav-link" href="formations">Gestion de payement</a>
+          						</li>
+         	    				<li class="nav-item">
+            						<a class="nav-link" href="contact">Gestion membres</a>
+            							<ul class="dropdown-menu">
+            								<li><a href="#">Inscription</a></li> 
+    										<li><a href="#">Participants</a></li> 
+    										<li><a href="#">Actions</a></li> 
+    										<li role="separator" class="divider"></li> 
+            							</ul>
+          						</li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="signinup">Connexion</a>
+                      </li>
+         					</ul>
+        				<form class="form-inline my-2 my-lg-0 pull-right">
+          					<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          					<button class="btn btn-outline-success my-2 my-sm-0" type="submit" >Search</button>
+        				</form>
+      					</div>
+    			</nav>
 
 			<script src="jquery-3.3.1.min.js"></script>
 			<script src="bootstrap/js/bootstrap.min.js"></script>
